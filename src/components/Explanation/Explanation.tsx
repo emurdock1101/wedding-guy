e3bd8c28b8ccc84a840543c7e6c195a03d1562a5
Murdoe1 <emurdock1101>
2022-07-05 17:47:06 -0400

    lining stuff up

--- src/components/Explanation/Explanation.tsx
+++ src/components/Explanation/Explanation.tsx
@@ -1,26 +1,16 @@
-import {Typography, makeStyles} from "@material-ui/core";
+import { Typography, makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: "10px"
-  },
-  info: {
-    marginBottom: "15px",
-    fontSize: 20
-  },
-  info2: {
-    marginBottom: "23px",
-    fontSize: 20
-  },
   category: {
-    marginBottom: "4vw",
+    marginBottom: "6.3vw",
     color: theme.palette.common.white,
     borderRadius: "10px",
     padding: "5px",
-    boxShadow: "2px 2px 2px grey"
+    boxShadow: "2px 2px 2px grey",
   },
   c1: {
     backgroundColor: theme.palette.error.light,
+    marginTop: "3.4vw",
   },
   c2: {
     backgroundColor: theme.palette.warning.light,
@@ -42,19 +32,7 @@ const Explanation: React.FC<ExplanationProps> = (props: ExplanationProps) => {
   const styles = useStyles();
 
   return (
-    <div className={styles.container}>
-      <Typography variant="subtitle1" className={styles.info}>
-        Your results are shown in the table and in the bar charts below.{" "}
-      </Typography>
-
-      <Typography variant="subtitle1" className={styles.info2}>
-        The Big 5 traits are Openness, Conscientiousness, Extraversion, Agreeableness, and
-        Compassion. Each of the Big 5 traits is composed of two subcategories known as the 10
-        Aspects. Each score is shown as a
-        percentile of the population. This means a score of 60% shows that you are higher in this
-        trait than 60% of people. The meaning of each trait can be different from the trait name's common use, so please be sure to read the interpretation section below.
-      </Typography>
-
+    <div>
       <Typography variant="subtitle1" className={`${styles.category}  ${styles.c1}`}>
         &#160;&#160;Openness (also referred to as "Openness to Experience") is composed of Openness
         and Intellect.
--- src/views/Results/Results.tsx
+++ src/views/Results/Results.tsx
@@ -7,21 +7,26 @@ import ReactApexChart from "react-apexcharts";
 import ResultTable from "../../components/ResultTable/ResultTable";
 
 export const useStyles = makeStyles((theme) => ({
-  info: {
-    padding: "1vw",
-  },
-  title: {
-    marginBottom: "0.1vw",
-    textAlign: "center",
+  container: {
+    padding: "10px",
   },
   subheading: {
     paddingTop: "10px",
     paddingBottom: "10px",
     textAlign: "center",
+    textDecoration: "underline",
   },
   chart: {
-    marginTop: "1vw"
-  }
+    marginTop: "1vw",
+  },
+  info1: {
+    marginBottom: "15px",
+    fontSize: 20,
+  },
+  info2: {
+    marginBottom: "23px",
+    fontSize: 20,
+  },
 }));
 
 interface ResultsProps {}
@@ -31,19 +36,39 @@ const Results: React.FC<ResultsProps> = (props: ResultsProps) => {
 
   return (
     <div>
-      <Grid container spacing={6} className={styles.info} justify="center" alignItems="flex-start">
+      <Grid
+        container
+        spacing={6}
+        className={styles.container}
+        justify="center"
+        alignItems="flex-start"
+      >
         <Grid item xs={12}>
           <Typography variant="h3" className={styles.subheading}>
             Results and explanation
           </Typography>
         </Grid>
+        <Grid item sm={10} lg={10}>
+          <Typography variant="subtitle1" className={styles.info1}>
+            Your results are shown in the table and in the bar charts below.{" "}
+          </Typography>
+
+          <Typography variant="subtitle1" className={styles.info2}>
+            The Big 5 traits are Openness, Conscientiousness, Extraversion, Agreeableness, and
+            Compassion. Each of the Big 5 traits is composed of two subcategories known as the 10
+            Aspects. Each score is shown as a percentile of the population. This means a score of
+            60% shows that you are higher in this trait than 60% of people. The meaning of each
+            trait can be different from the trait name's common use, so please be sure to read the
+            interpretation section below.
+          </Typography>
+        </Grid>
         <Grid item sm={10} lg={5}>
-          <Explanation  />
+          <Explanation />
         </Grid>
         <Grid item sm={10} lg={5}>
           <ResultTable />
         </Grid>
-        <Grid item xs={12} lg={5}  className={styles.chart}>
+        <Grid item xs={12} lg={5} className={styles.chart}>
           <ReactApexChart options={oceanOptions} series={oceanSeries} type="bar" height={550} />
         </Grid>
         <Grid item xs={12} lg={5} className={styles.chart}>
